d034fec38e35c674eecb43245649b7d102e3d6d1
Daniel Calvo <daniel.calvo@orain.io>
2024-11-03 19:03:06 +0100

    feat: Add health route to API router and create test settings file - Include 'health_routes' in the API router for additional endpoint. - Create 'test.py' settings file with SQLite configuration for testing.

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.routes import item_routes
+from app.api.v1.routes import health_routes, item_routes
 
 api_router = APIRouter()
+
+api_router.include_router(health_routes.router)
 api_router.include_router(item_routes.router, prefix="/items", tags=["items"])
--- app/db/session.py
+++
@@ -1,6 +0,0 @@
-from django.db import connections
-
-
-def get_db_connection(alias="default"):
-    connection = connections[alias]
-    return connection
